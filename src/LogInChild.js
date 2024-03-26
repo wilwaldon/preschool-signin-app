82e02004a8a03a6bdc8ae7fc47e0b43111603f20
William Waldon <wilwaldon@gmail.com>
2024-03-26 11:28:47 -0400

    login layout update. added image and shifted form to left of screen.

--- src/LogInChild.js
+++ src/LogInChild.js
@@ -23,7 +23,9 @@ const LogInChild = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto my-10 bg-white p-8 rounded-lg shadow-md">
+    <div className="container mx-auto p-4">
+  <div className="grid grid-cols-2 gap-4">
+    <div className="max-w-lg my-10 bg-white p-8">
       <ToastContainer />
       <h2 className="text-2xl font-bold text-center mb-6">Log In Child</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
@@ -34,7 +36,7 @@ const LogInChild = () => {
             id="childName"
             value={childName}
             onChange={(e) => setChildName(e.target.value)}
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+            className="mt-1 block w-full p-2 border border-gray-300"
             required
           />
         </div>
@@ -45,7 +47,7 @@ const LogInChild = () => {
             id="parentName"
             value={parentName}
             onChange={(e) => setParentName(e.target.value)}
-            className="mt-1 block w-full p-2 border border-gray-300 rounded-md"
+            className="mt-1 block w-full p-2 border border-gray-300"
             required
           />
         </div>
@@ -53,18 +55,23 @@ const LogInChild = () => {
           <label className="text-sm font-medium">Signature:</label>
           <SignatureCanvas
             penColor="black"
-            canvasProps={{className: 'sigCanvas mt-1 w-full border border-gray-300 rounded-md', height: 100}}
+            canvasProps={{className: 'sigCanvas mt-1 w-full border border-gray-300', height: 100}}
             ref={sigPad}
           />
         </div>
         <button
           type="submit"
-          className={`w-full py-2 rounded-md transition duration-300 ${isLoggedIn ? 'bg-gray-500' : 'bg-blue-500 hover:bg-blue-700 text-white'}`}
+          className={`w-full py-2  transition duration-300 ${isLoggedIn ? 'bg-gray-500' : 'bg-blue-500 hover:bg-blue-700 text-white'}`}
           disabled={isLoggedIn} // Disable button when logged in
         >
           {isLoggedIn ? 'Logged In' : 'Log In'} {/* Change button text based on login status */}
         </button>
       </form>
+    </div>
+    <div className="max-w-lg my-10 bg-white p-8">
+      <img src="https://images.unsplash.com/photo-1613950190144-4f2a84c75e8c?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="kids playing" className="h-48 w-full object-cover md:h-full" />
+    </div>
+    </div>
     </div>
   );
 };
